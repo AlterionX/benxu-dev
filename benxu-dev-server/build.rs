bcaf953738ab664a78df022209701d17fd864744
AlterionX <ben.xu.cs@gmail.com>
2019-07-20 21:46:31 -0700

    Clean the files before moving, just to make sure

--- benxu-dev-server/build.rs
+++ benxu-dev-server/build.rs
@@ -1,7 +1,6 @@
 use std::{ fs, path::Path, process::Command };
 
-fn main() {
-    let static_file_dir = Path::new("./public");
+fn build_dirs(root: &Path) {
     let needed_dirs = [
         "wasm-pack",
         "wasm",
@@ -11,19 +10,23 @@ fn main() {
         "img",
     ];
     for needed_dir in needed_dirs.iter() {
-        #[allow(unused_results, unused_must_use)]
-        fs::create_dir(static_file_dir.join(needed_dir)); // ignore
+        if let Err(e) = fs::create_dir(root.join(needed_dir)) {
+            println!("Directory not created! Error: {:?}", e);
+        }
     }
-    for entry in fs::read_dir(static_file_dir.join("wasm-pack")).unwrap() {
+}
+
+fn move_wasm_and_loader(root: &Path) {
+    for entry in fs::read_dir(root.join("wasm-pack")).unwrap() {
         let entry = entry.unwrap();
         let file = entry.file_name();
         if let Some(name) = file.to_str(){
             let path = entry.path();
             if name.ends_with(".js") {
-                fs::copy(&path, static_file_dir.join("js/wasm-bindgen-glue").join(name)).unwrap();
+                fs::copy(&path, root.join("js/wasm-bindgen-glue").join(name)).unwrap();
             }
             if name.ends_with(".wasm") {
-                let wasm_file_path = static_file_dir.join("wasm").join(name);
+                let wasm_file_path = root.join("wasm").join(name);
                 fs::copy(&path, &wasm_file_path).unwrap();
                 Command::new("wasm-gc")
                     .arg(&wasm_file_path)
@@ -33,3 +36,10 @@ fn main() {
         }
     }
 }
+
+fn main() {
+    let static_file_dir = Path::new("./public");
+    build_dirs(&static_file_dir);
+    move_wasm_and_loader(&static_file_dir);
+}
+
--- benxu-dev-server/build_for_deploy.sh
+++ benxu-dev-server/build_for_deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+rm -rf public/wasm
+rm -rf public/js/wasm-bindgen-glue
 cargo build --release --out-dir=. -Z unstable-options
